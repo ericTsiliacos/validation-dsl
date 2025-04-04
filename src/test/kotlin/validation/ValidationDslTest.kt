7b94c935f6dc16cb6563475f451a0f85c3c19133
Eric Tsiliacos <erictsiliacos@gmail.com>
2025-04-03 22:30:58 -0700

    Remove duplicated tests

--- src/test/kotlin/validation/ValidationDslTest.kt
+++ src/test/kotlin/validation/ValidationDslTest.kt
@@ -348,33 +348,6 @@ class ValidationDslTest {
     }
 
     @Test
-    fun `fromPredicate returns Valid when predicate passes`() {
-        val rule = fromPredicate<String>("field", "must not be blank") { it.isNotBlank() }
-        rule("abc").assertValid()
-    }
-
-    @Test
-    fun `fromPredicate returns Invalid when predicate fails`() {
-        val rule = fromPredicate<String>("field", "must not be blank") { it.isNotBlank() }
-        rule("").assertInvalid { errors ->
-            errors[0].assertMatches("field", "must not be blank")
-        }
-    }
-
-    @Test
-    fun `fromPredicate includes code when provided`() {
-        val rule = fromPredicate<String>(
-            path = "field",
-            message = "must not be blank",
-            code = "error.blank"
-        ) { it.isNotBlank() }
-
-        rule("").assertInvalid { errors ->
-            errors[0].assertMatches("field", "must not be blank", code = "error.blank")
-        }
-    }
-
-    @Test
     fun `predicate assigns path automatically when used in validation scope`() {
         val notBlank = predicate<String>("must not be blank") { it.isNotBlank() }
 
